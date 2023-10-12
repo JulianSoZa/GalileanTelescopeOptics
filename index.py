9069eac494bdc0bca77c81c4e7aa9314620fd719
Alejo <alejandro.delatorre.r@gmail.com>
2023-10-12 14:05:11 -0500

    Se mejoro el index para que la interfaz sea mejor

--- index.py
+++ index.py
@@ -1,20 +1,33 @@
 from modules import main, triplet, interpolation, analysisResults
 import pandas as pd
 
-print('Escoja el sistema de procesamiento: \n 1. Sistema Principal \n 2. Sistema Triplete \n 3. Interpolación \n 4. Gráficas')
-n = input()
 
-if(n == '1'):
-    main.main_system()
+d=True
+t=True
+while d==True:
+    print('Escoja el sistema de procesamiento: \n 1. Sistema Principal \n 2. Sistema Triplete \n 3. Interpolación \n 4. Gráficas')
+    n=input()
+    if(n == '1'):
+        main.main_system()
 
-elif(n == '2'):
-    triplet.triplet_system()
-    
-elif(n == '3'):
-    print("Elija la imagen a interpolar: \n 1. Imagen Aberrada \n 2. Imagen Sin Aberración")
-    m = int(input())
-    imgName = ['nuevaImagen', 'nuevaImagenAberracionCorregida', 'nuevaImagenAberradaInterpolada', 'nuevaImagenSinAberracionInterpolada']
-    interpolation.interpolation_system(imgName, m)
+    elif(n == '2'):
+        triplet.triplet_system()
+        
+    elif(n == '3'):
+        print("Elija la imagen a interpolar: \n 1. Imagen Aberrada \n 2. Imagen Sin Aberración")
+        m = int(input())
+        imgName = ['nuevaImagen', 'nuevaImagenAberracionCorregida', 'nuevaImagenAberradaInterpolada', 'nuevaImagenSinAberracionInterpolada']
+        interpolation.interpolation_system(imgName, m)
 
-elif(n == '4'):
-    analysisResults.figures()
+    elif(n == '4'):
+        analysisResults.figures()
+    else:
+        print('Por favor ingrese un valor valido')
+        t=False
+        pass
+    while t==True:
+        print('¿Desea continuar? \n y.  n.')
+        q=input()
+        if q=='n':
+            d=False
+        break
